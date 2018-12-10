8c79b17e0ca65023a06035afef6a99e3b7ed0ada
Dan Gopstein <dan@gopstein.com>
2018-12-09 22:56:00 -0500

    Horizontal card

--- edotor.js
+++ edotor.js
@@ -6,14 +6,14 @@ const [white, black, green, yellow, blue, red] =
        d3.rgb( 45, 192, 230),
        d3.rgb(227,  38, 38)];
 
-const n_cols = 6
-const n_rows = 12
+const n_rows = 6
+const n_cols = 12
 
 const buttons =
-      _.flatMap(_.range(n_rows), row =>
-                _.range(n_cols).map(col =>
-                                    ({row: row,
-                                      col: col,
+      _.flatMap(_.range(n_cols), col =>
+                _.range(n_rows).map(row =>
+                                    ({col: col,
+                                      row: row,
                                       eaten: false})));
 
 const buttonSize = 15;
@@ -26,43 +26,43 @@ const buttonColor = button => {
   } else {
     const colors = [green, yellow, blue, red];
 
-    return colors[_.floor(button.row/3)];
+    return colors[_.floor(button.col/3)];
   }
 }
 
-const rowY = row_idx => (row_idx+1)*buttonMargin;
+const colY = row_idx => (row_idx+1)*buttonMargin;
 const buttonX = b => (b.col+1)*buttonMargin
-const buttonY = b => rowY(b.row)
+const buttonY = b => colY(b.row)
 
 //////////////////////////////////////////////////////////////
 //                         BCD Card
 //////////////////////////////////////////////////////////////
 
-const buttonRows = () =>
-      _.map(_.groupBy(buttons, b => b.row), (v, k) => v)
-      .map(row => _.sortBy(row, b => b.col))
+const buttonCols = () =>
+      _.map(_.groupBy(buttons, b => b.col), (v, k) => v)
+      .map(col => _.sortBy(col, b => b.row))
 
 
 const gbcd =
 ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '[', '#', '@', ':', '>', '?',
- 'S', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', '&', '.', ']', '(', '<', '\\',
+ ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', '&', '.', ']', '(', '<', '\\',
  '^', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', '-', '$', '*', ')', ';', "'",
  '+', '/', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_', ',', '%', '=', '"', '!']
 const bcd = gbcd
 
-const buttonToInt = b => (b.eaten ? 0 : 1) * Math.pow(2, (n_cols-1) - b.col)
-const buttonRowToInt = buttonRow => _.sum(buttonRow.map(buttonToInt))
-const buttonRowToBCD = buttonRow => bcd[buttonRowToInt(buttonRow)]
+const buttonToInt = b => (b.eaten ? 0 : 1) * Math.pow(2, b.row)
+const buttonColToInt = buttonCol => _.sum(buttonCol.map(buttonToInt))
+const buttonColToBCD = buttonCol => bcd[buttonColToInt(buttonCol)]
 
 const buttonCard = d3.select("#button-card")
             .append("svg")
-            .attr("width", 350)
-            .attr("height", 651);
+            .attr("width", 651)
+            .attr("height", 350);
 
 const bcdCard = d3.select("#bcd-card")
             .append("svg")
-            .attr("width", 105)
-            .attr("height", 651);
+            .attr("width", 651)
+            .attr("height", 105);
 
 //buttonCard.on("click", function(b) { console.log("button card clicked") })
 
@@ -94,16 +94,16 @@ function bcdCardUpdate() {
 
   charSelector().remove()
 
-  const chars = charSelector().data(buttonRows)
+  const chars = charSelector().data(buttonCols)
 
   chars.enter()
     .merge(chars)
     .append("text")
     .attr("class", "char")
-    .attr("x", 10)
-    .attr("y", (d, i) => rowY(i))
-    .text(b => (buttonRowToInt(b).toString().padStart(2, " ") +
-                " => " + buttonRowToBCD(b)))
+    .attr("x", (d, i) => colY(i))
+    .attr("y", 27)
+    .text(b => //(buttonColToInt(b).toString().padStart(2, " ") + " => " +
+                buttonColToBCD(b))
     .attr("font-family", "courier")
     .attr("font-size", "20px")
 
