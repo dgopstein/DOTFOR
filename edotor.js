0f1ca8a0f9a86fca85bc416794eb81007b9bff03
Dan Gopstein <dan@gopstein.com>
2018-12-09 21:44:23 -0500

    BCD codes

--- edotor.js
+++ edotor.js
@@ -21,10 +21,13 @@ const buttonMargin = 50;
 
 
 const buttonColor = button => {
+  if (button.eaten) {
+    return white;
+  } else {
+    const colors = [green, yellow, blue, red];
 
-  const colors = [green, yellow, blue, red];
-
-  return colors[_.floor(button.row/3)];
+    return colors[_.floor(button.row/3)];
+  }
 }
 
 const rowY = row_idx => (row_idx+1)*buttonMargin;
@@ -39,8 +42,17 @@ const buttonRows = () =>
       _.map(_.groupBy(buttons, b => b.row), (v, k) => v)
       .map(row => _.sortBy(row, b => b.col))
 
-const buttonToInt = b => (b.eaten ? 0 : 1) * (n_cols - b.col)
+
+const gbcd =
+['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '[', '#', '@', ':', '>', '?',
+ 'S', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', '&', '.', ']', '(', '<', '\\',
+ '^', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', '-', '$', '*', ')', ';', "'",
+ '+', '/', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_', ',', '%', '=', '"', '!']
+const bcd = gbcd
+
+const buttonToInt = b => (b.eaten ? 0 : 1) * Math.pow(2, (n_cols-1) - b.col)
 const buttonRowToInt = buttonRow => _.sum(buttonRow.map(buttonToInt))
+const buttonRowToBCD = buttonRow => bcd[buttonRowToInt(buttonRow)]
 
 const buttonCard = d3.select("#button-card")
             .append("svg")
@@ -49,46 +61,50 @@ const buttonCard = d3.select("#button-card")
 
 const bcdCard = d3.select("#bcd-card")
             .append("svg")
-            .attr("width", 100)
+            .attr("width", 105)
             .attr("height", 651);
 
+//buttonCard.on("click", function(b) { console.log("button card clicked") })
+
 function buttonCardUpdate() {
-  const dots = buttonCard.selectAll(".dot")
-        .data(buttons.filter(b => !b.eaten))
+  const selectDots = () => buttonCard.selectAll(".dot")
+  const dots = selectDots()
+        .data(buttons)
 
   dots.enter()
     .append("circle")
+    .merge(dots)
+    .attr("class", "dot")
     .attr("cx",   buttonX)
     .attr("cy",   buttonY)
     .attr("r",    buttonSize)
-    .style("fill", buttonColor);
+    .style("fill", buttonColor)
 
   dots.exit().remove()
 
-  dots.on("click", function(b) {
-    b.eaten = !b.eaten
+  selectDots().on("click", function(b) {
+      b.eaten = !b.eaten
 
-    // Make it invisible before removing
-    if (b.eaten) {
-      d3.select(this)
-        .attr("r", buttonSize+1)
-        .style("fill", "white")
-    }
-
-    update()
-  })
+      update()
+    })
 }
 
 function bcdCardUpdate() {
-  const chars = bcdCard.selectAll(".char").data(buttonRows)
+  const charSelector = () => bcdCard.selectAll(".char")
+
+  charSelector().remove()
+
+  const chars = charSelector().data(buttonRows)
 
   chars.enter()
-    .append("text")
     .merge(chars)
+    .append("text")
+    .attr("class", "char")
     .attr("x", 10)
     .attr("y", (d, i) => rowY(i))
-    .text(buttonRowToInt)
-    .attr("font-family", "sans-serif")
+    .text(b => (buttonRowToInt(b).toString().padStart(2, " ") +
+                " => " + buttonRowToBCD(b)))
+    .attr("font-family", "courier")
     .attr("font-size", "20px")
 
   chars.exit().remove()
