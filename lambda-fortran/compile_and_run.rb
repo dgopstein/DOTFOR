677defeac27639361d5e0bd1ee508d775268625d
Dan Gopstein <dan@gopstein.com>
2018-12-11 11:33:42 -0500

    Rename dir/file

--- lambda-fortran/compile_and_run.rb
+++ lambda-fortran/compile_and_run.rb
@@ -1,14 +1,17 @@
 require 'json'
 require 'open3'
 
+RELATIVE_DIR = 'lambda-fortran'
+ABSOLUTE_DIR = '/var/task/' + RELATIVE_DIR
+
 def lambda_handler(event:, context:)
   source = event["source"]
 
   File.write('/tmp/prog.f90', source)
 
   cmpl_out, cmpl_err, cmpl_status = Open3.capture3(
-    {'LIBRARY_PATH' => '/var/task/lambda/g95/lib/dan-lib'},
-    "lambda/g95/bin/x86_64-unknown-linux-gnu-g95 /tmp/prog.f90 -o /tmp/prog")
+    {'LIBRARY_PATH' => ABSOLUTE_DIR+'/g95/lib/dan-lib'},
+    RELATIVE_DIR+'/g95/bin/x86_64-unknown-linux-gnu-g95 /tmp/prog.f90 -o /tmp/prog')
 
   execution = if cmpl_status.success?
                 Open3.capture3("/tmp/prog")
--- lambda-fortran/deploy_lambda.sh
+++ lambda-fortran/deploy_lambda.sh
@@ -1,6 +1,9 @@
-BASEDIR=$(dirname "$0")
+BASEDIR=`pwd`
+BASENAME=$(basename $BASEDIR)
+
+cd $BASEDIR/..
 
 rm $BASEDIR/build/lambda-fortran.zip
 
-zip -r $BASEDIR/build/lambda-fortran.zip $BASEDIR -x build/* -x .* &&
+zip -r $BASEDIR/build/lambda-fortran.zip $BASENAME -x build/* -x .* &&
     aws lambda update-function-code --profile personal --function-name arn:aws:lambda:us-east-1:239445984846:function:dotfor --zip-file fileb://$BASEDIR/build/lambda-fortran.zip
