470dd0b5cc2b35ad5ebd5895eaffbca189b5ea95
Dan Gopstein <dan@gopstein.com>
2018-12-16 19:38:47 -0500

    Measure common radii (useful) and common distances (not useful)

--- edotor/vision/vision.py
+++ edotor/vision/vision.py
@@ -2,6 +2,11 @@
 
 import numpy as np
 import cv2
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
+from sklearn.cluster import KMeans
+from sklearn.neighbors import KernelDensity
 
 # https://medium.com/@manivannan_data/resize-image-using-opencv-python-d2cdbbc480f0
 def scale(oriimg, imgScale):
@@ -26,19 +31,27 @@ hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
 hsv_channels = cv2.split(hsv);
 
 sat = hsv_channels[1]
+cv2.imshow('image', img)
 showImage(sat)
 destroyWindowOnKey()
 
 
 # find circles
 
-def detectCircles():
+def findCircles():
     hough_circles = cv2.HoughCircles(sat, cv2.HOUGH_GRADIENT, .8, 25,
                             param1=5,
                             param2=50,
-                            minRadius=3,
-                            maxRadius=50)
+                            #minRadius=10,
+                            #maxRadius=35
+                            minRadius=10,
+                            maxRadius=40
+    )
     circles = np.round(hough_circles[0, :]).astype("int")
+    print("finished detecting circles: ", len(circles))
+    return circles
+
+def displayCircles(circles):
     output = sat.copy()
 
     # loop over the (x, y) coordinates and radius of the circles
@@ -48,41 +61,47 @@ def detectCircles():
         cv2.circle(output, (x, y), r, (0, 5, 0), 4)
         cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
 
-    print("finished detecting circles: ", len(circles))
-
-    # show the output image
     showImage(np.hstack([sat, output]))
-    destroyWindowOnKey()
-
     print("closed window")
 
-def detectRect():
-    ret,thresh = cv2.threshold(sat,127,255,1)
+def hist(np_data, bins=20):
+    hist = pd.DataFrame(np_data).plot.hist(grid=True, bins=bins, rwidth=0.9, color='#607c8e')
+    hist.plot()
+    plt.show()
 
-    img,contours,h = cv2.findContours(thresh,1,2)
+circles = findCircles()
+displayCircles(circles)
+destroyWindowOnKey()
 
-    for cnt in contours:
-        approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-        print(len(approx))
-        if len(approx)==5:
-            print("pentagon")
-            cv2.drawContours(img,[cnt],0,255,-1)
-        elif len(approx)==3:
-            print("triangle")
-            cv2.drawContours(img,[cnt],0,(0,255,0),-1)
-        elif len(approx)==4:
-            print("square")
-            cv2.drawContours(img,[cnt],0,(0,0,255),-1)
-        elif len(approx) == 9:
-            print("half-circle")
-            cv2.drawContours(img,[cnt],0,(255,255,0),-1)
-        elif len(approx) > 15:
-            print("circle")
-            cv2.drawContours(img,[cnt],0,(0,255,255),-1)
+# most common radius (plus or minus a gaussian kernel)
+def radiiMode(circles):
+    estimated_radius = 25
+    max_radius_px = estimated_radius * 4
+    bandwidth = estimated_radius / 5
+    radii = circles[:, 2]
+    kde = KernelDensity(kernel="gaussian", bandwidth=bandwidth).fit(radii[:, np.newaxis])
+    x_grid = np.linspace(0, max_radius_px, radii.shape[0])
+    pdf = np.exp(kde.score_samples(x_grid[:, np.newaxis]))
+    #fig, ax = plt.subplots()
+    #ax.plot(x_grid, pdf, label="hello")
+    #plt.show()
+    return x_grid[np.argmax(pdf)]
 
-    showImage(np.hstack([sat, img]))
-    destroyWindowOnKey()
+def distMode(circles):
+    estimated_dist = 150
+    max_dist = estimated_dist * 5
+    bandwidth = estimated_dist / 5
+    crossproduct = [(c1, c2) for c1 in circles for c2 in circles]
+    dists = np.array([circle_dist(c1, c2) for (c1, c2) in circle_crossproduct])
+    kde = KernelDensity(kernel="gaussian", bandwidth=bandwidth).fit(dists[:, np.newaxis])
+    x_grid = np.linspace(0, max_dist, dists.shape[0])
+    pdf = np.exp(kde.score_samples(x_grid[:, np.newaxis]))
+    fig, ax = plt.subplots()
+    ax.plot(x_grid, pdf, label="hello")
+    plt.show()
+    return x_grid[np.argmax(pdf)]
 
-detectRect()
+def circle_dist(c1, c2):
+    return np.linalg.norm(c2-c1)
 
-1 + 3
+hist(circle_dists, 200)
