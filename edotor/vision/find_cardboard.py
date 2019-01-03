773ac4ea469e70cefce7cb34dfcce68853c02357
Dan Gopstein <dan@gopstein.com>
2019-01-03 18:41:15 -0500

    desperate attempts at generalization (not working so great)

--- edotor/vision/find_cardboard.py
+++ edotor/vision/find_cardboard.py
@@ -8,6 +8,7 @@ import math
 import itertools
 import scipy
 
+from sklearn.cluster import MeanShift
 # https://github.com/Aqsa-K/Car-Number-Plate-Detection-OpenCV-Python/blob/master/CarPlateDetection.py
 
 def scale(oriimg, imgScale):
@@ -42,6 +43,13 @@ def dilate_erode(img, kern = (2,2), iters=10):
 
 def blur(img):
     return cv2.GaussianBlur(img, (9, 9), 0)
+
+def drawPoints(in_img, points):
+    out_image=in_img.copy()
+    for pt in points:
+        cv2.circle(out_image,(int(pt[0]),int(pt[1])),7,[255,0,0],thickness=7)
+
+    return out_image
 
 def drawLines(in_img, lines, labels=[]):
     img = in_img.copy()
@@ -100,55 +108,66 @@ def seg_intersect(a1,a2, b1,b2) :
     return (num / denom.astype(float))*db + b1
 ###########################################################################
 
-img_path = '/Users/dgopstein/dotfor/edotor/vision/button_imgs/20181216_150759.jpg'
-#img_path = '/Users/dgopstein/dotfor/edotor/vision/button_imgs/20181216_150752.jpg'
+#img_path = '/Users/dgopstein/dotfor/edotor/vision/button_imgs/20181216_150759.jpg'
+img_path = '/Users/dgopstein/dotfor/edotor/vision/button_imgs/20181216_150752.jpg'
 
 image = imutils.resize(cv2.imread(img_path), width=1000)
 
+def lines_with_label_in(lines, labels, desired_labels):
+    return np.array([line for (line, label) in zip(lines, labels) if label in desired_labels])
+
+def eval_parametric(rho, theta, xs):
+    ys = -(np.array(xs)*np.cos(theta) - rho) / np.sin(theta)
+    return  np.array([list(pair) for pair in zip(xs, ys)]).reshape(-1,2)
+
 def card_corners(image):
     hue, sat, val = hsv_img(image)
-    val_sat = np.array((invert(sat)/255.0*val/255.0)*255, np.uint8)
-
-    # RGB to Gray scale conversion
-    gray=val_sat.copy()
-    showImage(gray)
+    val_sat = np.array((1.0*invert(sat)/255.0*val/255.0)*255*1, np.uint8)
+    showImage(val)
 
     # Noise removal with iterative bilateral filter
     # (removes noise while preserving edges)
-    gray = cv2.bilateralFilter(gray, 11, 17, 17)
+    gray = cv2.bilateralFilter(sat, 11, 17, 17)
 
     # Find Edges of the grayscale image
     edged = cv2.Canny(gray, 170, 200)
+    showImage(edged)
 
     # Turn the observed edges into inferred lines
-    lines = cv2.HoughLines(edged,rho=1,theta=np.pi/180,threshold=150)
+    lines = cv2.HoughLines(edged,rho=1,theta=np.pi/180,threshold=120)
     flat_lines = lines.reshape(lines.shape[0], lines.shape[2])
+
+    #showImage(drawLines(image, flat_lines))
 
     # only look at theta (angle) and mod by the quadrant to search for
     # rectangular things. This works since parallel lines already have
     # the same angle, and perpendicular lines will have the same angle
     # once modded by 90 degrees (pi/2 radians)
     quad_angles = flat_lines[:,1].reshape(-1,1) % (math.pi/2)
-    quad_clustering = sklearn.cluster.MeanShift(bandwidth=(math.pi/40)).fit(quad_angles)
+    quad_clustering = MeanShift(bandwidth=(.05)).fit(quad_angles)
 
     # select the lines that form the best box
     # (assumes the 0th cluster is the strongest)
-    boundary_lines = np.array([line for (line, label) in zip(flat_lines, quad_clustering.labels_) if label == 0])
+    quad_lines = lines_with_label_in(flat_lines, quad_clustering.labels_, [0])
+
+    showImage(drawLines(image, quad_lines))
+
+    #line_dists = quad_lines[:,0].reshape(-1,1)
+    #line_dists
+    #dist_clustering = MeanShift(bandwidth=(25)).fit(line_dists)
+    #showImage(drawLines(image, dist_lines, dist_clustering.labels_))
 
     # make sure the angles and intercepts are comparable
     # (normally they have very different domains)
     boundary_scaler = sklearn.preprocessing.MinMaxScaler(copy=True, feature_range=(0, 1))
-    scaled_boundaries = boundary_scaler.fit(boundary_lines).transform(boundary_lines)
+    scaled_boundaries = boundary_scaler.fit(quad_lines).transform(quad_lines)
 
     # we already have all the boundary lines
     # now determine which ones are for which edge
-    cardinal_clustering = sklearn.cluster.MeanShift(bandwidth=.05).fit(scaled_boundaries)
+    cardinal_clustering = MeanShift(bandwidth=.05).fit(scaled_boundaries)
     boundaries = boundary_scaler.inverse_transform(cardinal_clustering.cluster_centers_)
-    boundaries
 
-    def eval_parametric(rho, theta, xs):
-        ys = -(np.array(xs)*np.cos(theta) - rho) / np.sin(theta)
-        return  np.array([list(pair) for pair in zip(xs, ys)]).reshape(-1,2)
+    showImage(drawLines(image, boundaries))
 
     boundary_end_pts = [eval_parametric(boundary[0], boundary[1], [0, image.shape[1]]) for boundary in boundaries]
 
@@ -167,14 +186,14 @@ def card_corners(image):
     #    cv2.circle(out_image,(int(pt[0]),int(pt[1])),7,[255,0,0],thickness=7)
 
     #showImage(out_image)
-    #showImage(drawLines(image, boundaries))
+    showImage(drawLines(image, boundaries))
     #destroyWindowOnKey()
 
     return np.array(bounded_corners).reshape(-1,2)
 
 corners = card_corners(image)
 
-corners
+showImage(drawPoints(image, corners))
 
 width, height = 660, 250
 #width, height = 990, 325
